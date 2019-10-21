49556a474cfa525a6199956850e2b8dd53026f0a
kstro96 <alejandrocastro9b@hotmail.com>
2019-10-21 09:27:58 -0500

    Creación de permisos Clases y paquete Permissions

---
+++ app/src/main/java/com/example/ratatouille/Permissions/PermissionIds.java
@@ -0,0 +1,8 @@
+package com.example.ratatouille.Permissions;
+
+public class PermissionIds {
+    public static final  int REQUEST_READ_CONTACTS = 1;
+    public static final  int REQUEST_CAMERA = 2;
+    public static final  int REQUEST_LOCATION = 3;
+    public static final int REQUEST_READ_EXTERNAL_STORAGE = 4;
+}
