43136e79f935e04241f851edde7a0de00ff03168
Abel Cortés <abelsantiagocortes@gmail.com>
2019-10-21 10:08:56 -0500

    Rounded Images Tabs Chef

--- app/src/main/java/com/example/ratatouille/SliderFragment.java
+++ app/src/main/java/com/example/ratatouille/SliderFragment.java
@@ -12,25 +12,29 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.github.siyamed.shapeimageview.RoundedImageView;
+
 
 public class SliderFragment extends Fragment {
     View view;
-    ImageView image_chef;
+    RoundedImageView image_chef;
     TextView n_chef,loc_chef;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view=inflater.inflate(R.layout.fragment_c_slider,container,false);
-        image_chef=(ImageView) view.findViewById(R.id.img_landing);
-        n_chef=(TextView) view.findViewById(R.id.txt_nombreChef);
-        loc_chef=(TextView) view.findViewById(R.id.txt_locationChef);
+        image_chef= view.findViewById(R.id.img_chef);
+        n_chef= view.findViewById(R.id.txt_nombreChef);
+        loc_chef= view.findViewById(R.id.txt_locationChef);
         RelativeLayout backgrond= view.findViewById(R.id.background);
 
         if(getArguments()!=null){
-             n_chef.setText(getArguments().getString("NameChef"));
+            /*n_chef.setText(getArguments().getString("NameChef"));
             loc_chef.setText(getArguments().getString("LocChef"));
-            image_chef.setImageResource(getArguments().getInt("ImageChef"));
+            image_chef.setImageResource(getArguments().getInt("plate1_test.png"));
+            */
         }
+
         return view;
     }
 }
