fd19c9f50cb44fcdeabdcd8c8e1c9dd9a4c14608
kstro96 <alejandrocastro9b@hotmail.com>
2019-10-22 22:17:42 -0500

    Solicitud class

---
+++ app/src/main/java/com/example/ratatouille/Solicitud.java
@@ -0,0 +1,31 @@
+package com.example.ratatouille;
+
+public class Solicitud {
+    private String idCliente;
+    private String idChef;
+    private double distancia;
+
+    public String getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(String idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getIdChef() {
+        return idChef;
+    }
+
+    public void setIdChef(String idChef) {
+        this.idChef = idChef;
+    }
+
+    public double getDistancia() {
+        return distancia;
+    }
+
+    public void setDistancia(double distancia) {
+        this.distancia = distancia;
+    }
+}
